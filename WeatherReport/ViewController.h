e636d4c5b9bb2833c5d91534c227ec4c6fe19686
hhuang <hhuang@microstrategy.com>
2015-11-17 17:52:09 +0800

    Initial Commit

---
+++ WeatherReport/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherReport
+//
+//  Created by Huang Hongsen on 15/11/17.
+//  Copyright © 2015年 cn.daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
